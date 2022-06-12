22e053de44bc3a4e3abcc8033f5dac31c89f1d2d
Molla.Al <hope.miky1074@gmail.com>
2022-06-12 09:57:13 +0200

    working on simple changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,18 +59,19 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Tesfamichael Molla</h2>;
+  const three = <h3 className="big-heading">A talented backend engineer</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        {/* I’m a software engineer specializing in building (and occasionally designing) exceptional
+        digital experiences. Currently, I’m focused on building accessible, human-centered products */}
+        I am a software engineeer specializing in Backend (API) design and development. Currently,
+        I'm am contributing to highly scalable customer-centered products at{' '}
+        <a href="https://equitygroupholdings.com/ke/" target="_blank" rel="noreferrer">
+          Equity Bank Limited
+        </a>{' '}
+        as a Backend Engineer.
       </p>
     </>
   );
@@ -79,7 +80,8 @@ const Hero = () => {
       className="email-link"
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
-      rel="noreferrer">
+      rel="noreferrer"
+    >
       Check out my course!
     </a>
   );
