807beadc954ef4490b56858d4652a8e7a31724a4
Molla.Al <hope.miky1074@gmail.com>
2022-06-12 10:23:58 +0200

    working on about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'VueJS',
+    'Python',
+    'JavaScript',
+    'Django',
+    'LoopbackJS',
+    'Node.js',
+    'ReactJS',
+    'Flutter',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +144,39 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! I'm a software engineer based in Poland. I'm passionate about building highly
+              scalable and maintainable web applications. My interest in software engineering
+              started in the early 2000s when I started learning HTML for a high-school project. I
+              continued to learn HTML and CSS until I was introduced to C++ which I used to program
+              PIC and Arduino Micro-controllers back in college.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              I worked on Machine learning and Natural Language Processing projects for a few years
+              before I decided to switch to software development. Face recognintion,{' '}
+              <a
+                href="https://www.researchgate.net/publication/335684990_Self-driving_Delivery_Drone_power_by_AI_and_ROS"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Self-Driving drone
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              and a Smart Home System are some of my favorite projects I worked on when I was a
+              college student.
+            </p>
+
+            <p>
+              I am a team player and have a passion for learning new technologies and I'm always
+              looking for new opportunities to learn and grow. Currently I'm working on a project
+              called USSD Mobile Banking System for{' '}
+              <a
+                href="https://equitygroupholdings.com/ke/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Equity Bank Limited
+              </a>{' '}
+              which is a Nimiq blockchain core library.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
